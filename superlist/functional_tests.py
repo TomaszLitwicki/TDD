e2724ca1186f10098ba20dfd0f0872474e8eb686
Tomasz Litwicki <tomasz.litwicki@gmail.com>
2026-02-10 15:32:38 +0100

    First unittest.

--- superlist/functional_tests.py
+++ superlist/functional_tests.py
@@ -1,6 +1,34 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Strona główna aplikacji
+        self.browser.get('http://localhost:8000')
+
+        #Tytuł strony i nagłówek zawierają słowo "SHOPPING LIST"
+        self.assertIn('SHOPPING LIST', self.browser.title)
+        self.fail('Zakończenie testu')
+
+#Pole tekstowe do wpisania produktu
+#Wpisanie produktu: MLEKO
+#Enter
+#Aktualizacja strony
+#Wyświetlanie wpisanego produktu i pole tekstowe do wprowadzenia następnego
+#Wpisanie produktu: KAWA ROZPUSZCZALNA
+#Enter
+#Aktualizacja strony
+#Wyświetlanie wpisanych produktów i pole tekstowe do wprowadzenia kolejnego
+#Wygenerowany adres URL + info z instrukcją
+#Przejście pod wygenerowany LINK URL - wyświetla się lista zakupowa
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
