a034d0f43eb8e568824ff6f0485c266a58f1493b
Tomasz Litwicki <tomasz.litwicki@gmail.com>
2026-02-11 17:45:09 +0100

    Chapter 3

--- shoppinglist/tests.py
+++ shoppinglist/tests.py
@@ -1,5 +1,7 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
+
 from shoppinglist.views import home_page
 
 # It was my first test.
@@ -11,3 +13,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>SHOPPING LIST</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
