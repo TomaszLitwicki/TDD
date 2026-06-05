6af16946ae716ebdea0780de7f4ed52c2456f98b
Fenrir <tomasz.litwicki@gmail.com>
2026-06-05 22:39:09 +0200

    Refacto homepage view and used templates Django

--- functional_test.py
+++ functional_test.py
@@ -18,11 +18,12 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
+        
         header_text = self.browser.find_element(By.TAG_NAME, "h1").text
         self.assertIn("To-Do", header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element(By.Id, "id_new_element")
+        inputbox = self.browser.find_element(By.ID, "id_new_element")
         self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
         
         # She types "Buy peacock feathers" into a text box
--- lists/tests.py
+++ lists/tests.py
@@ -1,21 +1,25 @@
 from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
+# from django.http import HttpRequest
+# from lists.views import home_page
 
 # class SmokeTest(TestCase):
 #     def test_bad_maths(self):
 #         self.assertEqual(1+1, 2)
 
 class Home_Page_Test(TestCase):
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html : str = response.content.decode('utf8')
+    # def test_home_page_returns_correct_html(self):
+    #     request = HttpRequest()
+    #     response = home_page(request)
+    #     html : str = response.content.decode('utf8')
 
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.startswith("<html>"))
+    #     self.assertTrue(html.endswith("</html>"))
 
-    def test_home_page_returns_correct_html_2(self):
+    def test_uses_home_page_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, "home.html")
+    
+    def test_renders_home_page_content(self):
         response = self.client.get('/')
         self.assertContains(response, "<title>To-Do lists</title>")
