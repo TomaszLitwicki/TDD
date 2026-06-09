61ff13a69d4c96e68b39175c7163250ea00bee67
Fenrir <tomasz.litwicki@gmail.com>
2026-06-09 14:54:22 +0200

    ORM in Django and first migration of db

--- functional_test.py
+++ functional_test.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+    
+    def check_for_rows_in_table(self, data_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(data_text, [row.text for row in rows])
 
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
@@ -37,7 +42,7 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = self.browser.find_elements(By.TAG_NAME, "tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows]) 
+        self.check_for_rows_in_table("1: Buy peacock feathers") 
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -47,9 +52,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        rows = self.browser.find_elements(By.TAG_NAME, "tr")
-        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows]) 
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows]) 
+        self.check_for_rows_in_table("2: Use peacock feathers to make a fly") 
+        self.check_for_rows_in_table("1: Buy peacock feathers") 
 
         # Satisfied, she goes back to sleep
         self.fail("Finish the test!")
--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from lists.models import Item
 # from django.http import HttpRequest
 # from lists.views import home_page
 
@@ -33,3 +34,22 @@ class Home_Page_Test(TestCase):
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertContains(response, 'A new list item')
         self.assertTemplateUsed(response, "home.html")
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "The second item"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, "The first item")
+        self.assertEqual(second_saved_item.text, "The second item")
