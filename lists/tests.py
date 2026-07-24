4786323568a1cf555f23fea267222adf7ba481e9
Fenrir <tomasz.litwicki@gmail.com>
2026-07-24 20:01:51 +0200

    test with deleting migrations

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 # from django.http import HttpRequest
 # from lists.views import home_page
 
@@ -55,15 +55,23 @@ class Home_Page_Test(TestCase):
     #     self.client.get("/")
     #     self.assertEqual(Item.objects.count(), 0)
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        mylist = List()
+        mylist.save()
+
         first_item = Item()
-        first_item.text = "The first item"
+        first_item.text = "The first list item"
+        first_item.list = mylist
         first_item.save()
 
         second_item = Item()
         second_item.text = "The second item"
+        second_item.list = mylist
         second_item.save()
+
+        saved_list = List.objects.get()
+        self.assertEqual(saved_list, mylist)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -71,8 +79,10 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
 
-        self.assertEqual(first_saved_item.text, "The first item")
+        self.assertEqual(first_saved_item.text, "The first list item")
+        self.assertEqual(first_saved_item.list, mylist)
         self.assertEqual(second_saved_item.text, "The second item")
+        self.assertEqual(second_saved_item, mylist)
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
